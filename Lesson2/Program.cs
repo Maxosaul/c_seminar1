8f950af558db9e3eeeb679c0f8698caeb491ef84
osaulenko <osaulenkomax@gmail.com>
2023-10-06 17:02:50 +0300

    add lesson2

---
+++ Lesson2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число - ");
+int inputData0 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число - ");
+int inputData1 = int.Parse(Console.ReadLine());
+if (inputData1 == inputData0 * inputData0)
+{
+    Console.WriteLine("да");
+}
+else System.Console.Write("нет");
