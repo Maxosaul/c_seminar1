11150257dd2e398f60b064c33c0d495bab1239ab
osaulenko <osaulenkomax@gmail.com>
2023-10-06 16:39:47 +0300

    add lesson1

--- sem001/Program.cs
+++ sem001/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿Console.Write("Введите число => ");
+int inputData = int.Parse(Console.ReadLine());
+int result = inputData * inputData;
+System.Console.WriteLine("Квадрат числа " + inputData + " равен " + result);
